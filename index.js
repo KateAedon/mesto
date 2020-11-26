8eaced0e6e7a93aab250753f723a2d22fb5440d7
Ekaterina Aedonitskaia <katya@Ekaterinas-iMac.local>
2020-11-26 09:40:20 +0100

    First version of 4th project Mesto

---
+++ index.js
@@ -0,0 +1,41 @@
+const editButton = document.querySelector('.profile__edit_button');
+const closeButton = document.querySelector('.popup__close_button');
+let likeButtons = document.querySelectorAll('.card__info_button_like');
+let profileName = document.querySelector('.profile__text_name');
+let profileDescription = document.querySelector('.profile__text_description');
+const popup = document.querySelector('.popup');
+let nameInput = document.querySelector('.form__name');
+let descriptionInput = document.querySelector('.form__description');
+let formElement = document.querySelector('.popup__container');
+
+
+editButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
+
+function togglePopup() {
+    popup.classList.toggle('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = descriptionInput.value;
+    togglePopup();
+    closeButton.addEventListener('click', togglePopup); 
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+for (let i=0; i < likeButtons.length; i++) {
+    let likeButton = likeButtons[i];
+    likeButton.addEventListener('click', function (event) {  
+        event.preventDefault();
+
+        function toggleLikeButton() {
+            likeButton.classList.toggle('card__info_button_like_active');    
+        }
+
+        toggleLikeButton(this);
+
+    }, false);
+} 
