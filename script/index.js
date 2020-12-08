aff759178c9a59161a561aa68539249d8f008c45
Ekaterina Aedonitskaia <katya@Ekaterinas-iMac.local>
2020-12-08 10:57:54 +0100

    added template and popup for cards

--- script/index.js
+++ script/index.js
@@ -1,12 +1,29 @@
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close-button');
+const profileCloseButton = document.querySelector('.popup-profile__close-button');
 let likeButtons = document.querySelectorAll('.card__button-like');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
-const popup = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup-profile');
 let nameInput = document.querySelector('.form__input_type_name');
 let descriptionInput = document.querySelector('.form__input_type_description');
-let formElement = document.querySelector('.popup__container');
+let profileFormElement = document.querySelector('.popup-profile__container');
+
+function toggleProfilePopup() {
+    popupProfile.classList.toggle('popup_opened');
+    profileName.value = nameInput.textContent ;
+    profileDescription.value = descriptionInput.textContent;
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = descriptionInput.value;
+    toggleProfilePopup();
+}
+
+editButton.addEventListener('click', toggleProfilePopup);
+profileCloseButton.addEventListener('click', toggleProfilePopup);
+profileFormElement.addEventListener('submit', formSubmitHandler);
 
 const initialCards = [
     {
@@ -35,19 +52,24 @@ const initialCards = [
     }
 ];
 
-function togglePopup() {
-    popup.classList.toggle('popup_opened');
-    profileName.value = nameInput.textContent ;
-    profileDescription.value = descriptionInput.textContent;
+const popupCard = document.querySelector('.popup-card');
+const addButton = document.querySelector('.profile__add-button');
+const cardCloseButton = document.querySelector('.popup-card__close-button');
+const templateElement = document.querySelector('.card-template').content;
+const cardsContainer = document.querySelector('.cards__list');
+
+function makeCard(card) {
+    const newCard = templateElement.cloneNode(true);
+    newCard.querySelector('.card__image').src = card.link;
+    newCard.querySelector('.card__name').textContent = card.name;
+    cardsContainer.append(newCard);
 }
 
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileDescription.textContent = descriptionInput.value;
-    togglePopup();
+initialCards.forEach(makeCard);
+
+function toggleCardPopup() {
+    popupCard.classList.toggle('popup_opened');
 }
 
-editButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+addButton.addEventListener('click', toggleCardPopup);
+cardCloseButton.addEventListener('click', toggleCardPopup);
